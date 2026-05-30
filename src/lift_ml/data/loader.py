110317fda65c87880882325e97b9ae95d1bda673
muz <171587778+MuzammilAijaz@users.noreply.github.com>
2026-05-30 12:59:17 +0500

    refactor(data): move loader to data module

---
+++ src/lift_ml/data/loader.py
@@ -0,0 +1,125 @@
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0. See LICENSE for details.
+#******************************************************************************
+#  Data Loading and Preprocessing Module
+# -----------------------------------------------------------------------------
+#  Responsible for loading time-series CSV datasets from a directory structure,
+#  mapping class labels, and preparing data for model training.
+#  
+#  Main responsibilities:
+#    - Load CSV files from class-based folder structure
+#    - Convert raw sequences into numpy arrays
+#    - Apply optional data augmentation to training set
+#    - Pad sequences to fixed length
+#    - Convert datasets into tf.data.Dataset format
+#  
+#  Output:
+#    - Train / validation / test datasets ready for TensorFlow models
+#******************************************************************************
+
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from lift_ml.data.augment import augment_data
+
+# Labels
+label1 = "barbell"
+label2 = "none"
+DIMENSION = 2 
+
+class DataLoader(object):
+    """Loads CSV data and prepares for training."""
+
+    def __init__(self, train_path, valid_path, test_path, seq_length, augment_train=True):
+        self.dim = DIMENSION
+        self.seq_length = seq_length
+        self.label2id = {label1: 0, label2: 1}
+
+        # Load CSV data
+        self.train_data, self.train_label, self.train_len = self.load_csv_folder(train_path)
+        self.valid_data, self.valid_label, self.valid_len = self.load_csv_folder(valid_path)
+        self.test_data,  self.test_label,  self.test_len  = self.load_csv_folder(test_path)
+
+        # Augment training data if requested
+        if augment_train:
+            self.train_data, self.train_label = augment_data(self.train_data, self.train_label)
+            self.train_len = len(self.train_label)
+            print(f"After augmentation, train_data_length: {self.train_len}")
+
+    def load_csv_folder(self, root_path):
+        """ Load CSV files from a folder with subfolders for each class. """
+
+        data = []
+        labels = []
+
+        # Expected structure:
+        # root_path/
+        #    onBarbell/
+        #        file1.csv
+        #    notOnBarbell/
+        #        file2.csv
+
+        for label_name in os.listdir(root_path):
+            class_path = os.path.join(root_path, label_name)
+            if not os.path.isdir(class_path):
+                continue
+
+            for file in os.listdir(class_path):
+                if file.endswith(".csv"):
+                    file_path = os.path.join(class_path, file)
+                    df = pd.read_csv(file_path)
+
+                    # Expecting columns: ax, ay, az, gx, gy, gz
+                    if df.shape[1] != DIMENSION:
+                        raise ValueError(f"Invalid CSV format in {file_path}")
+
+                    # Convert to numpy [seq_len, 6]
+                    data.append(df.values)
+                    labels.append(label_name)
+
+        print(f"Loaded {len(labels)} samples from {root_path}")
+        return data, labels, len(labels)
+
+    def pad(self, data, seq_length, dim):
+        noise_level = 20
+        padded_data = []
+
+        # Before-padding
+        tmp = (np.random.rand(seq_length, dim) - 0.5) * noise_level + data[0]
+        tmp[(seq_length - min(len(data), seq_length)):] = data[:min(len(data), seq_length)]
+        padded_data.append(tmp)
+
+        # After-padding
+        tmp = (np.random.rand(seq_length, dim) - 0.5) * noise_level + data[-1]
+        tmp[:min(len(data), seq_length)] = data[:min(len(data), seq_length)]
+        padded_data.append(tmp)
+
+        return padded_data
+
+    def format_support_func(self, padded_num, length, data, label):
+        length *= padded_num
+        features = np.zeros((length, self.seq_length, self.dim))
+        labels = np.zeros(length)
+
+        for idx, (d, l) in enumerate(zip(data, label)):
+            padded_data = self.pad(d, self.seq_length, self.dim)
+            for num in range(padded_num):
+                features[idx * padded_num + num] = padded_data[num]
+                labels[idx * padded_num + num] = self.label2id[l]
+
+        ds = tf.data.Dataset.from_tensor_slices((features, labels.astype("int32")))
+        return length, ds
+
+    def format(self):
+        padded_num = 2
+        self.train_len, self.train_data = self.format_support_func(
+            padded_num, self.train_len, self.train_data, self.train_label)
+
+        self.valid_len, self.valid_data = self.format_support_func(
+            padded_num, self.valid_len, self.valid_data, self.valid_label)
+
+        self.test_len, self.test_data = self.format_support_func(
+            padded_num, self.test_len, self.test_data, self.test_label)
+
--- tests/data_load_test.py
+++ tests/data_load_test.py
@@ -22,74 +22,104 @@ from __future__ import division
 from __future__ import print_function
 
 import unittest
-from data_load import DataLoader
-
+import os
+import shutil
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 
+from lift_ml.data.loader import DataLoader
 
 class TestLoad(unittest.TestCase):
 
-  def setUp(self):  # pylint: disable=g-missing-super-call
-    self.loader = DataLoader(
-        "./data/train", "./data/valid", "./data/test", seq_length=512)
+    def setUp(self):
+        self.base_dir = "test_data_loader"
+        self.train_path = os.path.join(self.base_dir, "train")
+        self.valid_path = os.path.join(self.base_dir, "valid")
+        self.test_path = os.path.join(self.base_dir, "test")
 
-  def test_get_data(self):
-    self.assertIsInstance(self.loader.train_data, list)
-    self.assertIsInstance(self.loader.train_label, list)
-    self.assertIsInstance(self.loader.valid_data, list)
-    self.assertIsInstance(self.loader.valid_label, list)
-    self.assertIsInstance(self.loader.test_data, list)
-    self.assertIsInstance(self.loader.test_label, list)
-    self.assertEqual(self.loader.train_len, len(self.loader.train_data))
-    self.assertEqual(self.loader.train_len, len(self.loader.train_label))
-    self.assertEqual(self.loader.valid_len, len(self.loader.valid_data))
-    self.assertEqual(self.loader.valid_len, len(self.loader.valid_label))
-    self.assertEqual(self.loader.test_len, len(self.loader.test_data))
-    self.assertEqual(self.loader.test_len, len(self.loader.test_label))
+        # Create directories
+        for p in [self.train_path, self.valid_path, self.test_path]:
+            for cls in ["barbell", "none"]:
+                os.makedirs(os.path.join(p, cls), exist_ok=True)
 
-  def test_pad(self):
-    original_data1 = [[2, 3], [1, 1]]
-    expected_data1_0 = [[2, 3], [2, 3], [2, 3], [2, 3], [1, 1]]
-    expected_data1_1 = [[2, 3], [1, 1], [1, 1], [1, 1], [1, 1]]
-    original_data2 = [[-2, 3], [-77, -681], [5, 6], [9, -7], [22, 3333],
-                      [9, 99], [-100, 0]]
-    expected_data2 = [[-2, 3], [-77, -681], [5, 6], [9, -7], [22, 3333]]
-    padding_data1 = self.loader.pad(original_data1, seq_length=5, dim=2)
-    padding_data2 = self.loader.pad(original_data2, seq_length=5, dim=2)
-    for i in range(len(padding_data1[0])):
-      for j in range(len(padding_data1[0].tolist()[0])):
-        self.assertLess(
-            abs(padding_data1[0].tolist()[i][j] - expected_data1_0[i][j]),
-            10.001)
-    for i in range(len(padding_data1[1])):
-      for j in range(len(padding_data1[1].tolist()[0])):
-        self.assertLess(
-            abs(padding_data1[1].tolist()[i][j] - expected_data1_1[i][j]),
-            10.001)
-    self.assertEqual(padding_data2[0].tolist(), expected_data2)
-    self.assertEqual(padding_data2[1].tolist(), expected_data2)
+        # Create dummy CSV data (2 columns as per current DIMENSION=2)
+        data = np.random.rand(10, 2)
+        df = pd.DataFrame(data, columns=["ax", "ay"])
 
-  def test_format(self):
-    self.loader.format()
-    expected_train_label = int(self.loader.label2id[self.loader.train_label[0]])
-    expected_valid_label = int(self.loader.label2id[self.loader.valid_label[0]])
-    expected_test_label = int(self.loader.label2id[self.loader.test_label[0]])
-    for feature, label in self.loader.train_data:  # pylint: disable=unused-variable
-      format_train_label = label.numpy()
-      break
-    for feature, label in self.loader.valid_data:
-      format_valid_label = label.numpy()
-      break
-    for feature, label in self.loader.test_data:
-      format_test_label = label.numpy()
-      break
-    self.assertEqual(expected_train_label, format_train_label)
-    self.assertEqual(expected_valid_label, format_valid_label)
-    self.assertEqual(expected_test_label, format_test_label)
-    self.assertIsInstance(self.loader.train_data, tf.data.Dataset)
-    self.assertIsInstance(self.loader.valid_data, tf.data.Dataset)
-    self.assertIsInstance(self.loader.test_data, tf.data.Dataset)
+        df.to_csv(os.path.join(self.train_path, "barbell", "data1.csv"), index=False)
+        df.to_csv(os.path.join(self.valid_path, "barbell", "data1.csv"), index=False)
+        df.to_csv(os.path.join(self.test_path, "barbell", "data1.csv"), index=False)
+        df.to_csv(os.path.join(self.train_path, "none", "data1.csv"), index=False)
+        df.to_csv(os.path.join(self.valid_path, "none", "data1.csv"), index=False)
+        df.to_csv(os.path.join(self.test_path, "none", "data1.csv"), index=False)
 
+        self.loader = DataLoader(
+                self.train_path, self.valid_path, self.test_path, seq_length=5, augment_train=False)
+
+    def tearDown(self):
+        if os.path.exists(self.base_dir):
+            shutil.rmtree(self.base_dir)
+
+    def test_data_loader_loads_data_and_preserves_label_alignment(self):
+        self.assertIsInstance(self.loader.train_data, list)
+        self.assertIsInstance(self.loader.train_label, list)
+        self.assertIsInstance(self.loader.valid_data, list)
+        self.assertIsInstance(self.loader.valid_label, list)
+        self.assertIsInstance(self.loader.test_data, list)
+        self.assertIsInstance(self.loader.test_label, list)
+        self.assertEqual(self.loader.train_len, len(self.loader.train_data))
+        self.assertEqual(self.loader.train_len, len(self.loader.train_label))
+        self.assertEqual(self.loader.valid_len, len(self.loader.valid_data))
+        self.assertEqual(self.loader.valid_len, len(self.loader.valid_label))
+        self.assertEqual(self.loader.test_len, len(self.loader.test_data))
+        self.assertEqual(self.loader.test_len, len(self.loader.test_label))
+
+    def test_pad_preserves_last_value_during_sequence_expansion(self):
+        original_data1 = np.array([[2, 3], [1, 1]])
+        expected_data1_0 = [[2, 3], [2, 3], [2, 3], [2, 3], [1, 1]]
+        expected_data1_1 = [[2, 3], [1, 1], [1, 1], [1, 1], [1, 1]]
+        original_data2 = np.array([[-2, 3], [-77, -681], [5, 6], [9, -7], [22, 3333],
+                                   [9, 99], [-100, 0]])
+        expected_data2 = [[-2, 3], [-77, -681], [5, 6], [9, -7], [22, 3333]]
+        padding_data1 = self.loader.pad(original_data1, seq_length=5, dim=2)
+        padding_data2 = self.loader.pad(original_data2, seq_length=5, dim=2)
+
+        # Check padding with noise allowance
+        for i in range(len(padding_data1[0])):
+            for j in range(len(padding_data1[0][0])):
+                self.assertLess(
+                        abs(padding_data1[0][i][j] - expected_data1_0[i][j]),
+                        10.001)
+        for i in range(len(padding_data1[1])):
+            for j in range(len(padding_data1[1][0])):
+                self.assertLess(
+                        abs(padding_data1[1][i][j] - expected_data1_1[i][j]),
+                        10.001)
+
+        np.testing.assert_array_equal(padding_data2[0], expected_data2)
+        np.testing.assert_array_equal(padding_data2[1], expected_data2)
+
+    def test_format_converts_datasets_and_maps_labels_to_ids(self):
+        self.loader.format()
+        expected_train_label = int(self.loader.label2id[self.loader.train_label[0]])
+        expected_valid_label = int(self.loader.label2id[self.loader.valid_label[0]])
+        expected_test_label = int(self.loader.label2id[self.loader.test_label[0]])
+
+        # Get first batch
+        for feature, label in self.loader.train_data.take(1):
+            format_train_label = label.numpy()
+        for feature, label in self.loader.valid_data.take(1):
+            format_valid_label = label.numpy()
+        for feature, label in self.loader.test_data.take(1):
+            format_test_label = label.numpy()
+
+        self.assertEqual(expected_train_label, format_train_label)
+        self.assertEqual(expected_valid_label, format_valid_label)
+        self.assertEqual(expected_test_label, format_test_label)
+        self.assertIsInstance(self.loader.train_data, tf.data.Dataset)
+        self.assertIsInstance(self.loader.valid_data, tf.data.Dataset)
+        self.assertIsInstance(self.loader.test_data, tf.data.Dataset)
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
